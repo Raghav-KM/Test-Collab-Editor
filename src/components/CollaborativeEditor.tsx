f19c3240e77d6fdfe1febe7c436128d6dfc70386
Raghav <Raghav-KM>
2024-10-05 20:30:57 +0530

    F - Local Collaboration

--- src/components/CollaborativeEditor.tsx
+++ src/components/CollaborativeEditor.tsx
@@ -30,46 +30,34 @@ export const CollaborativeEditor = ({
         value = value;
         if (ev.isFlush) return;
         // Perform Operation on the Local CRDT
-        let op_id;
-        if (ev.changes[0].text != "") {
-            op_id = perform_normal_operation(
-                { ...root_crdt },
-                {
-                    pos: ev.changes[0].rangeOffset,
-                    value: ev.changes[0].text,
-                    type: "insert",
-                    priority: root_crdt.id.priority,
-                }
-            );
-            perform_crdt_operation(
-                { ...other_crdt },
-                {
-                    id: op_id,
-                    value: ev.changes[0].text,
-                    type: "insert",
-                }
-            );
-        } else {
-            op_id = perform_normal_operation(
-                { ...root_crdt },
-                {
-                    pos: ev.changes[0].rangeOffset,
-                    value: "",
-                    type: "delete",
-                    priority: root_crdt.id.priority,
-                }
-            );
-            perform_crdt_operation(
-                { ...other_crdt },
-                {
-                    id: op_id,
-                    value: "",
-                    type: "delete",
-                }
-            );
-        }
 
-        // Broadcast the operation
+        let op_id;
+        console.log("Local Operation : ", root_crdt.id.priority);
+        if (ev.changes[0].text != "") {
+            op_id = perform_normal_operation(root_crdt, {
+                pos: ev.changes[0].rangeOffset,
+                value: ev.changes[0].text,
+                type: "insert",
+                priority: root_crdt.id.priority,
+            });
+            perform_crdt_operation(other_crdt, {
+                id: op_id,
+                value: ev.changes[0].text,
+                type: "insert",
+            });
+        } else {
+            op_id = perform_normal_operation(root_crdt, {
+                pos: ev.changes[0].rangeOffset,
+                value: "",
+                type: "delete",
+                priority: root_crdt.id.priority,
+            });
+            perform_crdt_operation(other_crdt, {
+                id: op_id,
+                value: "",
+                type: "delete",
+            });
+        }
         other_editorRef.current?.setValue(get_character_sequence(other_crdt));
     };
 
